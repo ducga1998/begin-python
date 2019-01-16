982989b0cf260ab7c3232d2906a1623a257c40bf
Ducnm <duc2820@gmail.com>
2019-01-16 21:10:19 +0700

    begin learn basic python lesson 1

---
+++ first.py
@@ -0,0 +1,23 @@
+print "hello work "
+x = 5
+# practive number 
+print (x) # comment python
+print(type(x))
+print"type case check by dyc  "  , float("1.5")
+# practive string
+print (len( "string test"))
+# practive opertions
+y = 5
+y  = y + 5 
+print (y)
+# practive logical 
+flag = 2
+# 
+if flag  ==1 or flag == 2 :
+    print "flag ===1"
+elif flag == 2:
+    print "flag  ===2"
+else:
+    print "flag not as 1"
+
+
